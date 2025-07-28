b2344ee30c80131f4c0922d9ea70739cd7c8760b
Öykü Ünsay <oykunsay@gmail.com>
2025-07-28 19:36:25 +0300

    lesson 12

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,17 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-
-  const handleClick = (e) => {
-    console.log('hello ninjas', e);
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-  }
+  const [blogs, setBlogs] = useState([
+    {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+    {title: 'My new recipe', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+    {title: 'My new book', body: 'lorem ipsum...', author: 'sasha', id: 3}
+  ]);
+  
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+    <BlogList blogs={blogs} title="All Blogs!" />
     </div>
   );
 }
