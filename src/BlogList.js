90a814565c60c64ad7165608547a269951a8507f
Öykü Ünsay <oykunsay@gmail.com>
2025-07-30 17:33:59 +0300

    lesson 18

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,8 @@
-const BlogList = (props) => {
+const BlogList = ({blogs, title, handleDelete}) => {
 
-    const blogs = props.blogs;
-    const title = props.title;
-    console.log(props, blogs);
+   // const blogs = props.blogs;
+   // const title = props.title;
+ //   console.log(props, blogs);
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -10,6 +10,7 @@ const BlogList = (props) => {
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author}</p>
+                <button onClick={() => handleDelete(blog.id)}>delete blog</button>
             </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
     {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
     {title: 'My new recipe', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    {title: 'My new book', body: 'lorem ipsum...', author: 'sasha', id: 3}
+    {title: 'My new book', body: 'lorem ipsum...', author: 'mario', id: 3}
   ]);
   
+const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blogs => blogs.id != id);
+    setBlogs(newBlogs);
+}
+
+useEffect(() => {
+  console.log('use effect ran');
+  console.log(blogs);
+});
 
   return (
     <div className="home">
-    <BlogList blogs={blogs} title="All Blogs!" />
+    <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 }
